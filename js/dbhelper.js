abb98a4c70ceb667e6a95c9043df1150113584f2
Robin Savage <robin.savage@protonmail.com>
2019-03-01 23:00:16 -0600

    Test: Responsive images
    To-do: Add logic to dbhelper.js to grab picture container element width and include that in the equation.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,8 +150,8 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
     if (w <= 411 ) {
       return (`/img/resp_img/${restaurant.photograph}-small`);
