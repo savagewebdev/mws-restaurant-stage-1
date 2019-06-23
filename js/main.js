5f9f716397853239c6074a3d959aeb8156bbc5ba
Robin Savage <robin.savage@protonmail.com>
2019-06-23 18:19:42 -0500

    Changes image size parameters

--- js/main.js
+++ js/main.js
@@ -160,16 +160,16 @@ createRestaurantHTML = (restaurant) => {
   imago.className = 'restaurant-img';
   pic.alt = DBHelper.altForRestaurant(restaurant);
   const source_extrasmall = document.createElement('source');
-  source_extrasmall.media = "(max-width: 425px)"
+  source_extrasmall.media = "(max-width: 180px)"
   source_extrasmall.srcset = DBHelper.srcsetUrlForRestaurant(restaurant).xs;
   const source_small = document.createElement('source');
-  source_small.media = "(max-width: 768px)"
+  source_small.media = "(min-width: 181px) and (max-width: 410px)"
   source_small.srcset = DBHelper.srcsetUrlForRestaurant(restaurant).s;
   const source_medium = document.createElement('source');
-  source_medium.media = "(max-width: 1024px)"
+  source_medium.media = "(min-width: 411px) and (max-width: 650px)"
   source_medium.srcset = DBHelper.srcsetUrlForRestaurant(restaurant).m;
   const source_large = document.createElement('source');
-  source_large.media = "(min-width: 1025px)"
+  source_large.media = "(min-width: 651px)"
   source_large.srcset = DBHelper.srcsetUrlForRestaurant(restaurant).l;
   imago.src = DBHelper.imageUrlForRestaurant(restaurant);
 
