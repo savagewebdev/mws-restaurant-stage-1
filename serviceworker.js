4b984fe6aeb85979d5aa2957933ae70524e08151
Robin Savage <robin.savage@protonmail.com>
2019-07-06 09:11:40 -0500

    Streamlined image caching

--- serviceworker.js
+++ serviceworker.js
@@ -14,48 +14,13 @@ const filestoCache = [
     'js/dbhelper.js',
     'js/main.js',
     'js/restaurant_info.js',
-    'img/foodie.png',
-    'img/1.jpg',
-    'img/2.jpg',
-    'img/3.jpg',
-    'img/4.jpg',
-    'img/5.jpg',
-    'img/6.jpg',
-    'img/7.jpg',
-    'img/8.jpg',
-    'img/9.jpg',
-    'img/10.jpg',
-    'img/extrasmall/1.jpg',
-    'img/extrasmall/2.jpg',
-    'img/extrasmall/3.jpg',
-    'img/extrasmall/4.jpg',
-    'img/extrasmall/5.jpg',
-    'img/extrasmall/6.jpg',
-    'img/extrasmall/7.jpg',
-    'img/extrasmall/8.jpg',
-    'img/extrasmall/9.jpg',
-    'img/extrasmall/10.jpg',
-    'img/small/1.jpg',
-    'img/small/2.jpg',
-    'img/small/3.jpg',
-    'img/small/4.jpg',
-    'img/small/5.jpg',
-    'img/small/6.jpg',
-    'img/small/7.jpg',
-    'img/small/8.jpg',
-    'img/small/9.jpg',
-    'img/small/10.jpg',
-    'img/medium/1.jpg',
-    'img/medium/2.jpg',
-    'img/medium/3.jpg',
-    'img/medium/4.jpg',
-    'img/medium/5.jpg',
-    'img/medium/6.jpg',
-    'img/medium/7.jpg',
-    'img/medium/8.jpg',
-    'img/medium/9.jpg',
-    'img/medium/10.jpg',
-];
+    'img/foodie.png'
+  ];
+
+{
+for (i = 1; i <= 10; i++)
+  filestoCache.push(`img/${i}.jpg`, `img/extrasmall/${i}.jpg`, `img/small/${i}.jpg`, `img/medium/${i}.jpg`);
+}
 
 // Install the ServiceWorker.
 self.addEventListener('install', function(event) {
